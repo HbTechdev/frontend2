ec6ed1826f1191f8a2a45dcf448366e0baa24f3e
HbTechDev <hebertechdeveloper@gmail.com>
2022-03-23 20:35:43 -0300

    Adidionando outras aulas e Checkpoint 1

---
+++ aula-07/main.js
@@ -0,0 +1,19 @@
+let num1 = document.querySelector('#num1')
+let num2 = document.querySelector('#num2')
+
+
+function somar(num1, num2){
+    return num1 + num2
+}
+
+function subtrair(num1, num2){
+    return num1 - num2
+}
+
+function multiplicar(num1, num2){
+    return num1 * num2
+}
+
+function dividir(num1, num2){
+    return num1 / num2
+}
